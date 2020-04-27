e2f437be0d6db89761df7bf50fc31a02c977c436
steve <steve.williamson1@gmail.com>
2020-04-27 22:30:34 +0100

    20200417

---
+++ challenge1.py
@@ -0,0 +1,19 @@
+answer = 5
+
+print("Please guess a number between 1 and 10: ")
+guess = int(input())
+
+if guess == answer:
+    print("You got it first time")
+
+elif guess < answer:
+    print("Please guess higher")
+    guess = int(input())
+
+else:
+    print("Please guess lower")
+guess = int(input())
+if guess == answer:
+    print("Well done, you guessed it")
+else:
+    print("Sorry, you have not guessed correctly")
